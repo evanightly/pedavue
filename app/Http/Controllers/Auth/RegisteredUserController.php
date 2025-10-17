14915f3cc5b19c2558706f9eea1f4526b4f77a81
evanightly <adamhenderson3x3@gmail.com>
2025-10-17 13:57:18 +0700

    update: auto register student

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Support\Enums\RoleEnum;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -40,6 +41,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => $request->password,
         ]);
+
+        $user->assignRole(RoleEnum::Student);
 
         event(new Registered($user));
 
