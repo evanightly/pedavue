9cea9c675dbd5a05aee80ea85a4b145a5679d774
I <91296724+i9191@users.noreply.github.com>
2025-10-21 21:55:38 +0700

    feat: enhance QuizQuestionImport to support additional question formats and options handling

--- app/Imports/QuizQuestion/QuizQuestionImport.php
+++ app/Imports/QuizQuestion/QuizQuestionImport.php
@@ -19,7 +19,17 @@ class QuizQuestionImport implements ToCollection {
             empty($headers->search('Opsi 1 - Benar*')) ||
             empty($headers->search('Opsi 2*')) ||
             empty($headers->search('Opsi 3*'))) {
-            throw new \Exception('Format Excel tidak valid, pastikan kolom "Soal / Pertanyaan*", "Opsi 1 - Benar*", "Opsi 2*", dan "Opsi 3*" ada', 400);
+            if (empty($headers->search('Pertanyaan')) ||
+                empty($headers->search('Pilihan A')) ||
+                empty($headers->search('Pilihan B')) ||
+                empty($headers->search('Pilihan C')) ||
+                empty($headers->search('Pilihan D')) ||
+                empty($headers->search('Jawaban Benar'))) {
+                throw new \Exception('Format Excel tidak valid, pastikan Anda menggunakan format templat. Format setidaknya memiliki kolom "Soal / Pertanyaan*", "Opsi 1 - Benar*", "Opsi 2*", dan "Opsi 3*"', 400);
+            } else {
+                $this->useSpreadsheetFormat($rows);
+                return $this->quiz;
+            }
         }
         DB::beginTransaction();
         try {
@@ -81,5 +91,73 @@ class QuizQuestionImport implements ToCollection {
         }
 
         return $this->quiz;
+    }
+
+    private function useSpreadsheetFormat(Collection $rows) {
+        $headers = $rows[0];
+        DB::beginTransaction();
+        try {
+            $emptyCount = 0;
+            $questionAdded = 0;
+            $rows->skip(1)->take($rows->count() - 1)->each(function ($row) use ($headers, &$emptyCount, &$questionAdded) {
+                if (!empty($row[$headers->search('Pertanyaan')]) ||
+                    !empty($row[$headers->search('Pilihan A')]) ||
+                    !empty($row[$headers->search('Pilihan B')]) ||
+                    !empty($row[$headers->search('Pilihan C')]) ||
+                    !empty($row[$headers->search('Pilihan D')]) ||
+                    !empty($row[$headers->search('Jawaban Benar')])) {
+                    $emptyCount = 0;
+                } else {
+                    $emptyCount++;
+                    if ($emptyCount > 5) {
+                        DB::commit();
+                        return false;
+                    }
+                    return;
+                }
+                $question = $row[$headers->search('Pertanyaan')];
+                $quizQuestion = $this->quiz->quiz_questions()->updateOrCreate([
+                    'question' => $question,
+                ],[
+                    'order' => $questionAdded + 1
+                ]);
+                $questionAdded++;
+
+                $otherOptions = collect($row)->slice(2, 4)->filter(fn($v) => !empty($v));
+                $otherOptions->each(function ($option, $index) use ($quizQuestion, $headers, $row) {
+                    logger($index);
+                    $isCorrect = match($row[$headers->search('Jawaban Benar')]) {
+                        'A' => $index === (0 +2),
+                        'B' => $index === (1 +2),
+                        'C' => $index === (2 +2),
+                        'D' => $index === (3 +2),
+                    };
+                    $quizQuestion->quiz_question_options()->firstOrCreate([
+                        'option_text' => $option,
+                        'is_correct' => $isCorrect,
+                        'order' => $index - 1
+                    ]);
+                });
+
+                if ($this->is_answer_shuffled) {
+                    $option_order = 1;
+                    $quizQuestion->quiz_question_options()->inRandomOrder()->each(function ($options) use (&$option_order) {
+                        $options->order = $option_order++;
+                        $options->save();
+                    });
+                }
+            });
+            $questions = $this->quiz->quiz_questions();
+            $questions = $this->quiz->is_question_shuffled ? $questions->inRandomOrder() : $questions;
+            $question_order = $this->quiz->is_question_shuffled ? 1 : $questionAdded + 1;
+            $questions->orderBy('order')->skip($this->quiz->is_question_shuffled ? 0 : $questionAdded)->each(function ($question) use (&$question_order) {
+                $question->order = $question_order++;
+                $question->save();
+            });
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
     }
 }
